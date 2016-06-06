211c5220f6297c0ef07d6d0dd0ae6083935fcea1
Michael Minay <michael.minay@domandtom.com>
2016-06-06 21:18:57 +0800

    Small change for Project spacing in Nav Bar

--- lecet/ContactDetailViewController.m
+++ lecet/ContactDetailViewController.m
@@ -102,6 +102,7 @@
     NSNumber *fieldType = item[CONTACT_FIELD_TYPE];
     NSString *fieldData = item[CONTACT_FIELD_DATA];
     
+    NSLog(@"Email = %ld",(long)fieldType.integerValue);
     switch (fieldType.integerValue) {
         case ContactFieldTypePhone:{
             
