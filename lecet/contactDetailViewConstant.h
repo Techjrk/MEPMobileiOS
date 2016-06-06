77cee5b13acd1853b2b77d73a1856de5e8d8ed35
Michael Minay <michael.minay@domandtom.com>
2016-06-06 21:01:07 +0800

    Bottom border color for Contact Details view

---
+++ lecet/contactDetailViewConstant.h
@@ -0,0 +1,13 @@
+//
+//  contactDetailViewConstant.h
+//  lecet
+//
+//  Created by Michael San Minay on 06/06/2016.
+//  Copyright © 2016 Dom and TOm. All rights reserved.
+//
+
+#ifndef contactDetailViewConstant_h
+#define contactDetailViewConstant_h
+#define CONTACT_VIEW_BG_COLOR RGB(193, 193, 193)
+
+#endif /* contactDetailViewConstant_h */
