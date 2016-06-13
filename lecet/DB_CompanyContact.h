e91caa0beb03f5d0217a5ccb3e22d66ab463e933
Michael Minay <michael.minay@domandtom.com>
2016-06-13 20:42:15 +0800

    Merge and Fix Some Bug

--- lecet/DB_CompanyContact.h
+++ lecet/DB_CompanyContact.h
@@ -15,6 +15,7 @@ NS_ASSUME_NONNULL_BEGIN
 @interface DB_CompanyContact : DerivedNSManagedObject
 
 // Insert code here to declare functionality of your managed object subclass
+- (NSString *)fullAddress;
 
 @end
 
