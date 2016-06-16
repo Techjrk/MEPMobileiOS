1e49e231f476f92f9a9c6d7b27855c56a80fdf49
Michael Minay <michael.minay@domandtom.com>
2016-06-16 21:31:38 +0800

    Company Tracking Tab

---
+++ lecet/CompanyTrackingListView.m
@@ -0,0 +1,91 @@
+//
+//  CompanyTrackingListView.m
+//  lecet
+//
+//  Created by Michael San Minay on 16/06/2016.
+//  Copyright © 2016 Dom and TOm. All rights reserved.
+//
+
+#import "CompanyTrackingListView.h"
+#import "CompanyTrackingCollectionViewCell.h"
+
+@interface CompanyTrackingListView ()<UICollectionViewDelegate, UICollectionViewDataSource>{
+    NSMutableArray *collectionItems;
+    NSLayoutConstraint *constraintHeight;
+    CGFloat cellHeight;
+}
+@property (weak, nonatomic) IBOutlet UICollectionView *collectionView;
+
+@end
+
+@implementation CompanyTrackingListView
+#define kCellIdentifier             @"kCellIdentifier"
+
+- (void)awakeFromNib {
+    
+    [_collectionView registerNib:[UINib nibWithNibName:[[CompanyTrackingCollectionViewCell class] description] bundle:nil] forCellWithReuseIdentifier:kCellIdentifier];
+    
+}
+
+- (void)setItems:(NSMutableArray*)items {
+    collectionItems = items;
+    constraintHeight.constant = 0;
+    _collectionView.delegate = self;
+    _collectionView.dataSource = self;
+    [_collectionView reloadData];
+}
+
+#pragma mark - UICollectionView source and delegate
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+    
+    CompanyTrackingCollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:kCellIdentifier forIndexPath:indexPath];
+    
+
+    
+    return cell;
+}
+
+
+- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView {
+    return 1;
+}
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+    
+    //NSInteger count = collectionItems.count;
+    return 4;
+}
+
+- (CGSize)collectionView:(UICollectionView *)collectionView
+                  layout:(UICollectionViewLayout *)collectionViewLayout
+  sizeForItemAtIndexPath:(NSIndexPath *)indexPath {
+    CGSize size;
+    cellHeight = kDeviceHeight * 0.13;
+    size = CGSizeMake( _collectionView.frame.size.width, cellHeight);
+    return size;
+}
+
+- (UIEdgeInsets)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout insetForSectionAtIndex:(NSInteger)section;
+{
+    return UIEdgeInsetsMake(0, 0, kDeviceHeight * 0.015, 0);
+}
+
+- (CGFloat)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout minimumLineSpacingForSectionAtIndex:(NSInteger)section
+{
+    return 0;
+}
+
+- (CGFloat)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout minimumInteritemSpacingForSectionAtIndex:(NSInteger)section
+{
+    return 0;
+}
+
+- (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath {
+    
+   
+    
+}
+
+
+@end
