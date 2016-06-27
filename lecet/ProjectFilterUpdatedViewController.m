f1569f8fc95e89853f68fe86fc6579216145ce92
Michael Minay <michael.minay@domandtom.com>
2016-06-27 16:30:34 +0800

    Change and remove Constant

--- lecet/ProjectFilterUpdatedViewController.m
+++ lecet/ProjectFilterUpdatedViewController.m
@@ -9,7 +9,18 @@
 #import "ProjectFilterUpdatedViewController.h"
 #import "ProjectFilterSelectionViewList.h"
 #import "ProfileNavView.h"
-#import "projectFilterSelectionConstant.h"
+
+typedef enum  {
+    ProjectFilterItemAny = 0,
+    ProjectFilterItemHours = 1,
+    ProjectFilterItemDays = 2,
+    ProjectFilterItemMonths = 3,
+} ProjectFilterItem;
+
+
+#define PROJECT_SELECTION_TITLE @"TITLE"
+#define PROJECT_SELECTION_VALUE @"VALUE"
+#define PROJECT_SELECTION_TYPE  @"TYPE"
 
 @interface ProjectFilterUpdatedViewController ()<ProfileNavViewDelegate,ProjectFilterSelectionViewListDelegate>
 @property (weak, nonatomic) IBOutlet ProfileNavView *navView;
