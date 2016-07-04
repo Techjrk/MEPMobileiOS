a57656bd25d0f0ba5f5d2806d3a037e3ab52815a
Michael Minay <michael.minay@domandtom.com>
2016-07-05 00:08:16 +0800

    Simulating Error

---
+++ lecet/ProjectFilterSelectionViewController.h
@@ -0,0 +1,38 @@
+//
+//  ProjectFilterSelectionViewController.h
+//  lecet
+//
+//  Created by Michael San Minay on 04/07/2016.
+//  Copyright © 2016 Dom and TOm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+/*
+typedef enum  {
+    ProjectFilterItemAny = 0,
+    ProjectFilterItemHours = 1,
+    ProjectFilterItemDays = 2,
+    ProjectFilterItemMonths = 3,
+} ProjectFilterItem;
+
+#define PROJECT_SELECTION_TITLE @"TITLE"
+#define PROJECT_SELECTION_VALUE @"VALUE"
+#define PROJECT_SELECTION_TYPE  @"TYPE"
+
+
+@protocol ProjectFilterSelectionViewControllerDelegate <NSObject>
+- (void)tappedApplyButton:(id)items;
+
+@end
+*/
+
+@interface ProjectFilterSelectionViewController : UIViewController
+/*
+@property (nonatomic,assign) id <ProjectFilterSelectionViewControllerDelegate> projectFilterSelectionViewControllerDelegate;
+@property (nonatomic,weak) NSArray *dataInfo;
+@property (nonatomic,weak) NSString *navTitle;
+@property (nonatomic,weak) NSString *navRightButtonTitle;
+ */
+ 
+@end
+
