586f8e7aaf80817829ad3c872ba6bf8adf637148
Michael Minay <michael.minay@domandtom.com>
2017-03-14 17:18:04 +0800

    Implemente FONT and COLOR for Mobile Project Note

--- lecet/MobileProjectNotePopUp/MobileProjectNotePopUpViewController.m
+++ lecet/MobileProjectNotePopUp/MobileProjectNotePopUpViewController.m
@@ -8,6 +8,24 @@
 
 #import "MobileProjectNotePopUpViewController.h"
 
+
+#define PROJECTDETAIL_CANCEL_BUTTON_FONT_COLOR              RGB(0, 63, 144)
+
+
+#pragma mark - FONT
+#define FONT_PN_POPUP_TITLE                                 fontNameWithSize(FONT_NAME_LATO_SEMIBOLD, 13)
+#define FONT_POSTNOTE_BUTTON_TITLE                          fontNameWithSize(FONT_NAME_LATO_BOLD, 11)
+#define FONT_POSTNOTE_CANCEL_BUTTON                         fontNameWithSize(FONT_NAME_LATO_BOLD, 10)
+
+
+#pragma mark - COLOR
+#define COLOR_FONT_POPUP_TITLE                              RGB(33, 33, 33)
+
+#define COLOR_BG_POSTNOTE_BUTTON                            RGB(0, 63, 114)
+#define COLOR_FONT_POSTNOTE_BUTTON_TITLE                    RGB(255, 255, 255)
+#define COLOR_FONT_POSTNOTE_CANCEL_BUTTON                   RGB(0, 63, 144)
+
+
 @interface MobileProjectNotePopUpViewController ()
 @property (weak, nonatomic) IBOutlet UILabel *titleLabel;
 @property (weak, nonatomic) IBOutlet UIButton *postNoteButton;
@@ -22,8 +40,19 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
     self.titleLabel.text = NSLocalizedLanguage(@"MPNPV_TITLE");
+    self.titleLabel.font = FONT_PN_POPUP_TITLE;
+    self.titleLabel.textColor = COLOR_FONT_POPUP_TITLE;
+    
     [self.postNoteButton setTitle:NSLocalizedLanguage(@"MPNPV_POST_NOTE") forState:UIControlStateNormal];
+    [self.postNoteButton setTitleColor:COLOR_FONT_POSTNOTE_BUTTON_TITLE forState:UIControlStateNormal];
+    self.postNoteButton.titleLabel.font = FONT_POSTNOTE_BUTTON_TITLE;
+    self.postNoteButton.backgroundColor = COLOR_BG_POSTNOTE_BUTTON;
+    
     [self.cancelButton setTitle:NSLocalizedLanguage(@"MPNPV_CANCEL") forState:UIControlStateNormal];
+    [self.cancelButton setTitleColor:COLOR_FONT_POSTNOTE_CANCEL_BUTTON forState:UIControlStateNormal];
+    self.cancelButton.titleLabel.font = FONT_POSTNOTE_CANCEL_BUTTON;
+  
+    
 }
 
 - (void)didReceiveMemoryWarning {
@@ -31,14 +60,5 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
