dbe65988c4207b4f891a33fe8081be83b5edcfb7
Harry Herrys Camigla <harry.camigla@domandtom.com>
2017-03-16 10:12:49 -0400

    Commit changes in PhotoViewController

--- lecet/PhotoViewController.h
+++ lecet/PhotoViewController.h
@@ -7,7 +7,8 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "BaseViewController.h"
 
-@interface PhotoViewController : UIViewController
+@interface PhotoViewController : BaseViewController
 
 @end
--- lecet/PhotoViewController.m
+++ lecet/PhotoViewController.m
@@ -11,7 +11,8 @@
 @interface PhotoViewController ()<UIScrollViewDelegate>
 @property (weak, nonatomic) IBOutlet UIImageView *imageView;
 @property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
-
+@property (weak, nonatomic) IBOutlet UIButton *buttonBack;
+@property (weak, nonatomic) IBOutlet UITextView *textView;
 @end
 
 @implementation PhotoViewController
@@ -19,18 +20,46 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    UIImage *image = [UIImage imageNamed:@"splashImage"];
+    UIImage *image = [UIImage imageNamed:@"landscape.jpg"];
+    //UIImage *image = [UIImage imageNamed:@"splashImage"];
+
     self.imageView.image = image;
-    self.imageView.frame = self.scrollView.frame;
+    
+    CGRect frame = CGRectZero;
+    
+    if (image.size.width>image.size.height) {
+        CGFloat height = (1-((image.size.width-image.size.height)/image.size.width)) * kDeviceWidth;
+        frame = CGRectMake(0, (kDeviceHeight - height)/2.0, kDeviceWidth, height);
+    } else {
+        CGFloat width = (1-((image.size.height-image.size.width)/image.size.height)) * kDeviceHeight;
+        
+        CGFloat yPos = 0;
+        if (image.size.height<kDeviceHeight) {
+            yPos = (kDeviceHeight-image.size.height)/2;
+        }
+        
+        frame = CGRectMake(0, yPos, width, kDeviceHeight - (yPos*2));
+    }
+    
+    self.imageView.frame = frame;
     [self.scrollView addSubview:self.imageView];
     self.scrollView.minimumZoomScale=1.0;
     self.scrollView.maximumZoomScale = 6.0;
     self.scrollView.zoomScale = 1.0;
     self.scrollView.contentSize = self.imageView.frame.size;
+    
+    self.buttonBack.hidden = YES;
+    self.textView.hidden = YES;
+    self.textView.backgroundColor = [self.textView.backgroundColor colorWithAlphaComponent:0.5];
+    [self enableTapGesture:YES];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
+}
+
+- (BOOL)prefersStatusBarHidden {
+    return YES;
 }
 
 #pragma mark - IBAction
@@ -43,6 +72,34 @@
 
 - (UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView {
     return self.imageView;
+}
+
+#pragma mark - TapGesture
+
+- (void)handleSingleTap:(UITapGestureRecognizer *)sender {
+
+    UIView *view = sender.view;
+    CGPoint point = [sender locationInView:view];
+    UIView* subview = [view hitTest:point withEvent:nil];
+    
+    if ([subview isEqual:self.scrollView]) {
+        self.buttonBack.alpha = self.buttonBack.hidden?0:1;
+        self.buttonBack.hidden = !self.buttonBack.hidden;
+        
+        self.textView.alpha = self.buttonBack.alpha;
+        self.textView.hidden = self.buttonBack.hidden;
+        
+        [UIView animateWithDuration:0.25 animations:^{
+            
+            self.buttonBack.alpha = self.buttonBack.hidden?0:1;
+            self.textView.alpha = self.buttonBack.alpha;
+            
+        } completion:^(BOOL finished) {
+            if (finished) {
+                
+            }
+        }];
+    }
 }
 
 @end
