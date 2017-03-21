005df8674a40d28b742ef5503161ff3a5b763e38
Harry Herrys Camigla <harry.camigla@domandtom.com>
2017-03-21 10:07:20 -0400

    Commit NewProjectViewController

---
+++ lecet/NewProjectViewController.m
@@ -0,0 +1,29 @@
+//
+//  NewProjectViewController.m
+//  lecet
+//
+//  Created by Harry Herrys Camigla on 3/21/17.
+//  Copyright © 2017 Dom and TOm. All rights reserved.
+//
+
+#import "NewProjectViewController.h"
+
+@interface NewProjectViewController ()
+
+@end
+
+@implementation NewProjectViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+- (BOOL)automaticallyAdjustsScrollViewInsets {
+    return YES;
+}
+
+@end
