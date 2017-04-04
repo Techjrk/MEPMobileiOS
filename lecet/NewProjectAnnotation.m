03e1fb8118d2c953a9b137aab9acf23c7be1f21d
Harry Herrys Camigla <harry.camigla@domandtom.com>
2017-04-04 06:11:31 -0400

    Commit new project pin

---
+++ lecet/NewProjectAnnotation.m
@@ -0,0 +1,13 @@
+//
+//  NewProjectAnnotation.m
+//  lecet
+//
+//  Created by Harry Herrys Camigla on 4/4/17.
+//  Copyright © 2017 Dom and TOm. All rights reserved.
+//
+
+#import "NewProjectAnnotation.h"
+
+@implementation NewProjectAnnotation
+
+@end
---
+++ lecet/NewProjectAnnotationView.m
@@ -0,0 +1,13 @@
+//
+//  NewProjectAnnotationView.m
+//  lecet
+//
+//  Created by Harry Herrys Camigla on 4/4/17.
+//  Copyright © 2017 Dom and TOm. All rights reserved.
+//
+
+#import "NewProjectAnnotationView.h"
+
+@implementation NewProjectAnnotationView
+
+@end
