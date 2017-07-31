663e19922c4782b8aad32795d273d6efaa5f79c0
Harry Herrys Camigla <harry.camigla@domandtom.com>
2017-07-31 08:22:32 -0400

    Commit customview controller

---
+++ ProjectNearMeUI/ProjectNearIntentViewController.h
@@ -0,0 +1,13 @@
+//
+//  ProjectNearIntentViewController.h
+//  lecet
+//
+//  Created by Harry Herrys Camigla on 7/31/17.
+//  Copyright © 2017 Dom and TOm. All rights reserved.
+//
+
+#import <IntentsUI/IntentsUI.h>
+
+@interface ProjectNearIntentViewController : UIViewController <INUIHostedViewControlling>
+
+@end
---
+++ ProjectNearMeUI/ProjectNearIntentViewController.m
@@ -0,0 +1,42 @@
+//
+//  ProjectNearIntentViewController.m
+//  lecet
+//
+//  Created by Harry Herrys Camigla on 7/31/17.
+//  Copyright © 2017 Dom and TOm. All rights reserved.
+//
+
+#import "ProjectNearIntentViewController.h"
+
+@interface ProjectNearIntentViewController ()
+
+@end
+
+@implementation ProjectNearIntentViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - INUIHostedViewControlling
+
+// Prepare your view controller for the interaction to handle.
+- (void)configureWithInteraction:(INInteraction *)interaction context:(INUIHostedViewContext)context completion:(void (^)(CGSize))completion {
+    // Do configuration here, including preparing views and calculating a desired size for presentation.
+    
+    if (completion) {
+        completion([self desiredSize]);
+    }
+}
+
+- (CGSize)desiredSize {
+    return [self extensionContext].hostedViewMaximumAllowedSize;
+}
+
+@end
