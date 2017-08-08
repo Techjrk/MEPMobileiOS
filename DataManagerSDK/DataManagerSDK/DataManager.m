36fe16b5708ed37ef99a5544d7b980382e585fc1
Harry Herrys Camigla <harry.camigla@domandtom.com>
2017-08-08 11:39:37 -0400

    Commit change for recently updated

--- DataManagerSDK/DataManagerSDK/DataManager.m
+++ DataManagerSDK/DataManagerSDK/DataManager.m
@@ -780,7 +780,7 @@
     
     NSDate *previousMonth = [DerivedNSManagedObject getDate:[NSDate date] daysAhead:-(numberOfDays)];
     
-    NSString *filter = [NSString stringWithFormat:@"{\"include\":[\"projectStage\", {\"primaryProjectType\":{\"projectCategory\":\"projectGroup\"}}], \"where\":{\"lastPublishDate\":{\"gte\":\"%@\"}}, \"limit\":250,\"dashboardTypes\":true,\"order\":\"firstPublishDate DESC\"}", [DerivedNSManagedObject dateStringFromDateDay:previousMonth]];
+    NSString *filter = [NSString stringWithFormat:@"{\"include\":[\"projectStage\", {\"primaryProjectType\":{\"projectCategory\":\"projectGroup\"}}], \"where\":{\"lastPublishDate\":{\"gte\":\"%@\"}}, \"limit\":250,\"dashboardTypes\":true,\"order\":\"lastPublishDate DESC\"}", [DerivedNSManagedObject dateStringFromDateDay:previousMonth]];
 
     
     [self HTTP_GET:[self url:kUrlBidsRecentlyUpdated] parameters:@{@"filter":filter} success:^(id object) {
