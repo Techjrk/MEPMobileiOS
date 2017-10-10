64350e58190f6c0909a6f6c259749815ce798994
Harry Herrys Camigla <harry.camigla@domandtom.com>
2017-10-10 05:14:09 -0400

    bug fixes

---
+++ lecet/LecetApplication.m
@@ -0,0 +1,17 @@
+//
+//  LecetApplication.m
+//  lecet
+//
+//  Created by Harry Herrys Camigla on 10/10/17.
+//  Copyright © 2017 Dom and TOm. All rights reserved.
+//
+
+#import "LecetApplication.h"
+
+@implementation LecetApplication
+
+- (BOOL)isStatusBarHidden {
+    return YES;
+}
+
+@end
--- lecet/main.m
+++ lecet/main.m
@@ -8,9 +8,9 @@
 
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
-
+#import "LecetApplication.h"
 int main(int argc, char * argv[]) {
     @autoreleasepool {
-        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+        return UIApplicationMain(argc, argv, NSStringFromClass([LecetApplication class]), NSStringFromClass([AppDelegate class]));
     }
 }
